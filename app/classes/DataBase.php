d10af75bba165d25a330898d1725c096a6a2eb29
Aleksey Abramov <capit@LAPTOP-HCOMIG65>
2018-05-01 17:42:33 +0300

    Вторая финальная версия. Исправление замечаний.

--- app/classes/DataBase.php
+++ app/classes/DataBase.php
@@ -1,19 +1,24 @@
 <?php
 
 namespace diplomApp\classes;
-class DataBase
-{
-    /**
+/**
      * Подключение к базе данных mysql
      * @param $host адрес
      * @param $dbname название базы
      * @param $user пользователь
      * @param $pass пароль
      */
-    public static function connect($host, $dbname, $user, $pass)
+class DataBase
+{
+    private static $host = 'localhost';
+    private static $dbname = 'diplom';
+    private static $user = 'root';
+    private static $pass = '';
+
+    public static function connect()
     {
         try {
-            $db = new \PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $pass);
+            $db = new \PDO('mysql:host='. self::$host.';dbname='. self::$dbname.';charset=utf8', self::$user, self::$pass);
             } catch (PDOException $e) {
                 die('Database error: '.$e->getMessage().'<br/>');
             }
--- app/start.php
+++ app/start.php
@@ -3,13 +3,7 @@
 $router = new \diplomApp\core\Router();
 
 //Подключаемся к базе данных
-$config = new \diplomApp\Config();
-$db = \diplomApp\classes\DataBase::connect(
-        $config['mysql']['host'],
-        $config['mysql']['dbname'],
-        $config['mysql']['user'],
-        $config['mysql']['pass']
-);
+$db = \diplomApp\classes\DataBase::connect();
 
 //Запускаем роутер
 $router->start($db);
