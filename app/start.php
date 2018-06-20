104e45ec624b09a64dab8750f27977ea5736d58f
Aleksey Abramov <>
2018-06-20 22:35:48 +0300

    Вторая финальная версия. Исправление замечаний.

--- app/start.php
+++ app/start.php
@@ -3,9 +3,11 @@
 $router = new \diplomApp\core\Router();
 
 //Подключаемся к базе данных
+if(!file_exists('../app/config.json')) {
+            throw new \Exception ('Отсутствует файл app/config.json с настройками подключения к БД');
+        }
 $file_config = file_get_contents('../app/config.json');
 $config = json_decode($file_config, TRUE);
-var_dump($config);
 $db = \diplomApp\classes\DataBase::connect($config);
 
 //Запускаем роутер
